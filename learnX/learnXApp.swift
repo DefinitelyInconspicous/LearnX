b2e1a492a2635b6a458e31284f109bc75ee9c456
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2023-09-19 19:45:11 +0800

    Initial Commit

---
+++ learnX/learnXApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnXApp.swift
+//  learnX
+//
+//  Created by Avyan Mehra on 19/9/23.
+//
+
+import SwiftUI
+
+@main
+struct learnXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
