c2c054a13a2a3ffd6ce8a2dabe9acef21adb9b3f
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2023-09-23 16:50:18 +0800

    Made the note and search feature compatible with each other. Thanks @CheeseTan!

--- learnX/Notes/NotesMain.swift
+++ learnX/Notes/NotesMain.swift
@@ -18,13 +18,13 @@ struct note: Identifiable, Codable, Hashable {
 }
 struct NotesMain: View {
     // Filtering search results
-    @State private var searchResults: [note] {
-           if search.isEmpty {
-               return noteList
-           } else {
-               return noteList.filter { $0.noteTitle.lowercased().contains(search.lowercased()) }
-           }
-    }
+    private var searchResults: Binding<[note]> {
+               if search.isEmpty {
+                   return $noteList
+               } else {
+                   return .constant($noteList.wrappedValue.filter { $0.noteTitle.lowercased().contains(search.description.lowercased()) })
+               }
+        }
 
     @State var Sheet: Bool = false
     // Creating the placeholder of note(A default)
@@ -37,19 +37,21 @@ struct NotesMain: View {
     func remove(at offsets: IndexSet) {
         noteList.remove(atOffsets: offsets)
     }
-    @State var search: String
+    @State var search: String = ""
     var body: some View {
         // Nesting the navigation stack, List for the notes and VStack for the description of the notes
         
         NavigationStack {
             // Adds the note view to the list for every note in noteList
             List {
-                ForEach($searchResults, id: \.self) { note in
+                ForEach(searchResults, id: \.self) { note in
                     NavigationLink {
                         NoteView(name: note.noteTitle, what: note.noteContent , date: note.noteCreated)
                     } label: {
                         // The Label is the noteTitle and the Date of creation
                         VStack(alignment: .leading) {
+                            
+                            
                             Text(note.noteTitle.wrappedValue)
                                 .font(.title)
                                 .fontWeight(.medium)
@@ -98,9 +100,9 @@ struct NotesMain: View {
    
             
         }
-    struct NotesMain_Previews: PreviewProvider {
-        static var previews: some View {
-            NotesMain( search: "")
-        }
+    }
+struct NotesMain_Previews: PreviewProvider {
+    static var previews: some View {
+        NotesMain()
     }
 }
